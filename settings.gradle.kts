5427e25a5a9bcf7b0e3cd4ebe3034d0e1173366c
DmiMukh <mukhachevdp@yandex.ru>
2024-06-23 21:03:22 +0700

    Initial

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+ pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+rootProject.name = "Stock System"
+include(":app")
+include(":core")
