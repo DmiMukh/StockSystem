128dd6eabb4b3c9fe477444ce6c7f268234ee9b6
Ruslan <noname0263@gmail.com>
2024-06-25 14:26:32 +0700

    add CategoryClientKtor.kt

--- core/src/main/java/ru/hackathone/core/inventoryApi/staff/clientsImpl/CategoryClientKtor.kt
+++ core/src/main/java/ru/hackathone/core/inventoryApi/staff/clientsImpl/CategoryClientKtor.kt
@@ -1,4 +1,30 @@
 package ru.hackathone.core.inventoryApi.staff.clientsImpl
 
-class CategoryClientKtor {
+import ru.hackathone.core.inventoryApi.staff.clientsInterfaces.CategoryClient
+import ru.hackathone.core.inventoryApi.staff.dto.Category
+
+class CategoryClientKtor : CategoryClient {
+    override suspend fun create(category: Category) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(id: Int, category: Category) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAll() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getById(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getByName(name: String) {
+        TODO("Not yet implemented")
+    }
 }
