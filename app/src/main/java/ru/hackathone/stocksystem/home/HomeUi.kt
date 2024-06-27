85d24cf0134ba8c1fbc98072c4f990e8d235515d
DmiMukh <mukhachevdp@yandex.ru>
2024-06-27 11:19:17 +0700

    add buttons

--- app/src/main/java/ru/hackathone/stocksystem/home/HomeUi.kt
+++ app/src/main/java/ru/hackathone/stocksystem/home/HomeUi.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +14,6 @@ import androidx.compose.ui.unit.sp
 import ru.hackathone.core.theme.AppTheme
 import ru.hackathone.stocksystem.home.toolbar.HomeToolbarUi
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeUi(component: HomeComponent) {
     Scaffold (
@@ -26,6 +24,15 @@ fun HomeUi(component: HomeComponent) {
                 .fillMaxWidth()
                 .padding(paddingValues)
         ) {
+            MenuButton(
+                title = "Product",
+                onClick = {}
+            )
+
+            MenuButton(
+                title = "Staff",
+                onClick = {}
+            )
         }
     }
 }
