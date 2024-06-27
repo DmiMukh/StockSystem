8d75e2b22cebc7ca1d5b7fbd9d1f8b0aec5fb750
Ruslan <noname0263@gmail.com>
2024-06-28 04:16:59 +0700

    add serialization

--- core/src/main/java/ru/hackathone/core/inventoryApi/staff/models/Staff.kt
+++ core/src/main/java/ru/hackathone/core/inventoryApi/staff/models/Staff.kt
@@ -1,5 +1,7 @@
 package ru.hackathone.core.inventoryApi.staff.models
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Staff(
     val id: Int,
     val login: String,
--- core/src/main/java/ru/hackathone/core/inventoryApi/staff/models/Task.kt
+++ core/src/main/java/ru/hackathone/core/inventoryApi/staff/models/Task.kt
@@ -1,5 +1,8 @@
 package ru.hackathone.core.inventoryApi.staff.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Task(
     val id: Int,
     val title: String,
