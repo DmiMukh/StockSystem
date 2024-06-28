f2adb68efbf99b8203156dc90e12b8de86cca3b7
Ruslan <noname0263@gmail.com>
2024-06-28 14:03:27 +0700

    add tests for Staff client and service

---
+++ core/src/test/java/ru/hackathone/core/inventoryapi/staff/StaffClientTest.kt
@@ -0,0 +1,86 @@
+package ru.hackathone.core.inventoryapi.staff
+
+import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import ru.hackathone.core.inventoryApi.staff.client.StaffClientImpl
+import ru.hackathone.core.inventoryApi.staff.dto.TaskRequest
+import ru.hackathone.core.provideKtorHttpClient
+
+class StaffClientTest {
+    private val client = StaffClientImpl(provideKtorHttpClient(), addr = "http://localhost:8090")
+
+    @Test
+    fun createTask() = runBlocking {
+        val response = client.createTask(
+            TaskRequest(
+                "Add feature",
+                "You have to add feature in the ims-staff application",
+                1,
+                0
+            )
+        )
+
+        assertEquals(response.status, HttpStatusCode.OK)
+    }
+
+
+    @Test
+    fun updateTask() = runBlocking {
+        val response = client.updateTask(
+            0,
+            TaskRequest(
+                "Add feature",
+                "Just write some tests",
+                1,
+                0
+            )
+        )
+        assertEquals(response.status, HttpStatusCode.OK)
+    }
+
+    @Test
+    fun deleteTask() = runBlocking {
+        val response = client.deleteTask(
+            0,
+            1
+        )
+        assertEquals(response.status, HttpStatusCode.OK)
+    }
+
+    @Test
+    fun getTaskList() = runBlocking {
+        val response = client.getTaskList()
+        assertEquals(response.status, HttpStatusCode.OK)
+    }
+
+    @Test
+    fun getStaffList() = runBlocking {
+        val response = client.getStaffList()
+        assertEquals(response.status, HttpStatusCode.OK)
+    }
+
+    @Test
+    fun getStatusList() = runBlocking {
+        val response = client.getStatusList()
+        assertEquals(response.status, HttpStatusCode.OK)
+    }
+
+    @Test
+    fun getTaskListByStatus() = runBlocking {
+        val response = client.getTaskListByStatus("")
+        assertEquals(response.status, HttpStatusCode.OK)
+    }
+
+    @Test
+    fun getWorkerTasksById() = runBlocking {
+        val response = client.getWorkerTasksById(0)
+        assertEquals(response.status, HttpStatusCode.OK)
+    }
+
+    @Test
+    fun assignTask() {
+        //val response = client.assignTask()
+    }
+}
---
+++ core/src/test/java/ru/hackathone/core/inventoryapi/staff/StaffServiceTest.kt
@@ -0,0 +1,109 @@
+package ru.hackathone.core.inventoryapi.staff
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import ru.hackathone.core.inventoryApi.staff.client.StaffClientImpl
+import ru.hackathone.core.inventoryApi.staff.dto.TaskRequest
+import ru.hackathone.core.inventoryApi.staff.service.StaffServiceImpl
+import ru.hackathone.core.provideKtorHttpClient
+
+class StaffServiceTest {
+    private val client = StaffClientImpl(provideKtorHttpClient(), addr = "http://localhost:8090")
+    private val service = StaffServiceImpl(client)
+    private val task1 = TaskRequest(
+        "Add feature",
+        "You have to add feature in the ims-staff application",
+        1,
+        0
+    )
+    private val task2 = TaskRequest(
+        "Add feature",
+        "Just write some tests",
+        2,
+        0
+    )
+    //@Test
+    //fun assignTask()
+
+    @Test
+    fun getLists() = runBlocking {
+        val taskList = service.getTaskList()
+        println(taskList)
+
+        val staffList = service.getStaffList()
+        println(staffList)
+
+        val statusList = service.getStatusList()
+        println(statusList)
+
+        assertEquals(1, 2)
+    }
+
+    @Test
+    fun getTaskListByStatus() = runBlocking {
+        val taskList = service.getTaskListByStatus(statusName = "Wait")
+        println(taskList)
+
+        for (task in taskList) {
+            assertEquals(1, task.statusId)
+        }
+    }
+
+    @Test
+    fun getWorkerTasksById() = runBlocking {
+        val taskList = service.getWorkerTasksById(0)
+        println(taskList)
+
+        for (task in taskList) {
+            assertEquals(1, 2)
+        }
+    }
+
+    @Test
+    fun createTask() = runBlocking {
+        val beforeTaskList = service.getTaskList()
+        val before = beforeTaskList.size
+
+        service.createTask(task1)
+        val afterTaskList = service.getTaskList()
+        val after = afterTaskList.size
+
+        println(beforeTaskList)
+        println(afterTaskList)
+
+        assertEquals(before + 1, after)
+    }
+
+    @Test
+    fun updateTask() = runBlocking {
+        val beforeTaskList = service.getTaskList()
+        val beforeSize = beforeTaskList.size
+        println(beforeTaskList)
+
+        service.createTask(task1)
+        service.updateTask(beforeSize, task2)
+
+        val afterTaskList = service.getTaskList()
+        val after = afterTaskList.size
+        println(afterTaskList)
+
+        assertEquals(afterTaskList[after - 1], task2)
+    }
+
+    @Test
+    fun deleteTask() = runBlocking {
+        val beforeTaskList = service.getTaskList()
+        val beforeSize = beforeTaskList.size
+        println(beforeTaskList)
+
+        service.createTask(task1)
+        service.deleteTask(beforeSize, task1.statusId)
+
+        val afterTaskList = service.getTaskList()
+        val afterSize = afterTaskList.size
+        println(afterTaskList)
+
+        assertEquals(beforeSize, afterSize)
+    }
+}
